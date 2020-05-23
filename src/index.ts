8044c5ba32dfa5c6db38588fa70d14839e7eed86
Cortland Mahoney <cortland.mahoney@gmail.com>
2020-05-23 18:05:21 -0400

    Added pink floor

--- src/index.ts
+++ src/index.ts
@@ -1,141 +1,162 @@
 // copy paste imports 
-import { Engine } from "@babylonjs/core/Engines/engine";
-import { Scene } from "@babylonjs/core/scene";
-import { Vector3 } from "@babylonjs/core/Maths/math";
-import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
-import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
-import { Mesh } from "@babylonjs/core/Meshes/mesh";
-import { GridMaterial } from "@babylonjs/materials/grid";
-import * as TEXTURES from '@babylonjs/core/Materials/Textures';
+import { Engine } from "@babylonjs/core/Engines/engine"
+import { Scene } from "@babylonjs/core/scene"
+import { Vector3 } from "@babylonjs/core/Maths/math"
+import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera"
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight"
+import { Mesh } from "@babylonjs/core/Meshes/mesh"
+import { GridMaterial } from "@babylonjs/materials/grid"
+import "@babylonjs/core/Materials/standardMaterial"
+import "@babylonjs/core/Meshes/Builders/sphereBuilder"
+import "@babylonjs/core/Meshes/Builders/boxBuilder"
 
-import { Color3 } from '@babylonjs/core/Maths/math.color';
 
 // my imports
-// import * as TEXTURES from "@babylonjs/core/Materials/Textures"; 
-// import from "@babylonjs/core/Materials/Textures"; 
-import { StandardMaterialDefines } from "@babylonjs/core/Materials/standardMaterial";
-import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
-import { MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder";
+import * as TEXTURES from '@babylonjs/core/Materials/Textures'
+import { Color3 } from '@babylonjs/core/Maths/math.color'
+import { StandardMaterialDefines } from "@babylonjs/core/Materials/standardMaterial"
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial"
+import { MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder"
 
-// Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
-// Side-effects only imports allowing the standard material to be used as default.
-import "@babylonjs/core/Materials/standardMaterial";
-// Side-effects only imports allowing Mesh to create default shapes (to enhance tree shaking, the construction methods on mesh are not available if the meshbuilder has not been imported).
-import "@babylonjs/core/Meshes/Builders/sphereBuilder";
-import "@babylonjs/core/Meshes/Builders/boxBuilder";
-
-const GROUND_SIZE = 600;
-const GROUND_DEPTH = 2;
-
-init();
+// @ts-ignore
+const GROUND_SIZE = Number.parseFloat(600)
+// @ts-ignore
+const GROUND_DEPTH = Number.parseFloat(2)
 
 
+init()
+
+
+/** Create and run the game environment */
 function init() {
-  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
-  const engine = new Engine(canvas);
-  const scene = setupEnvironment( new Scene(engine) );
-
-
-  const sphere = createSphere(scene);
-  engine.runRenderLoop( () => render(scene, sphere) );
+  const canvas = document.querySelector("canvas") as HTMLCanvasElement
+  const engine = new Engine(canvas)
+  const scene = createGardenScene(engine);
+  const sphere = createSphere(scene)
+  engine.runRenderLoop( () => render(scene, sphere) )
 }
 
 
+/** Create a new camera attached to `scene` */
 function createCamera(name, position, scene): FreeCamera {
-  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
-  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-  camera.setTarget(Vector3.Zero());
-  camera.attachControl(canvas, true);
-  return camera;
+  const canvas = document.querySelector("canvas") as HTMLCanvasElement
+  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene)
+  camera.setTarget(Vector3.Zero())
+  camera.attachControl(canvas, true)
+  return camera
 }
 
 
-function setupEnvironment(scene: Scene): Scene {
-  const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
-  const camera = createCamera("camera1", new Vector3(0, 5, -10), scene);
-  const skybox = createSkybox(scene, 'skybox');
-  return scene;
-}
 
 
+/** Creates a sphere within a `scene` */
 function createSphere(scene: Scene): Mesh {
-  const greenCloth = new StandardMaterial("greenCloth", scene);
-  const sphere = MeshBuilder.CreateSphere("sphere1", {segments: 16, diameter: 4}, scene);
+  const greenCloth = new StandardMaterial("greenCloth", scene)
+  const sphere = MeshBuilder.CreateSphere("sphere1", {segments: 16, diameter: 4}, scene)
 
-  sphere.position.y = 2;
+  sphere.position.y = 2
 
-  greenCloth.diffuseColor = new Color3( 0, 0, 1 );
-  greenCloth.specularColor = new Color3( 0, 0, 1 );
-  greenCloth.emissiveColor = new Color3( 1, 0, 1 );
-  greenCloth.ambientColor = new Color3( 1, 0, 1 );
+  greenCloth.diffuseColor = new Color3( 0, 0, 1 )
+  greenCloth.specularColor = new Color3( 0, 0, 1 )
+  greenCloth.emissiveColor = new Color3( 1, 0, 0 )
+  greenCloth.ambientColor = new Color3( 1, 0, 0 )
 
-  sphere.material = greenCloth;
-  return sphere;
+  sphere.material = greenCloth
+  return sphere
 }
 
 
-function createGround( scene ) {
-  // const material = new GridMaterial("grid", scene);
-  const ground = MeshBuilder.CreateGround("ground1", {width: GROUND_SIZE, height: GROUND_SIZE, subdivisions: GROUND_DEPTH}, scene);
-  const material = new StandardMaterial('ground', scene);
-  material.diffuseColor = new Color3(1, 0, 1);
-  material.specularColor = new Color3(0.5, 0.6, 0.87);
-  material.emissiveColor = new Color3(1, 1, 1);
-  material.ambientColor = new Color3(0.23, 0.98, 0.53);
+/** Initialize static environmental for a garden */
+function createGardenScene(engine) {
+  const scene = new Scene(engine)
+  const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene)
+  const camera = createCamera("camera1", new Vector3(0, 5, -10), scene)
+  const skybox = createSkybox(scene, 'skybox')
+  const ground = createGround(scene, 'ground')
+
+  // @ts-ignore
+  console.log('created ground', window.ground = ground)
+  return scene
 }
 
 
-function createSkybox( scene, name = 'skybox' ): Mesh {
-  const skybox = MeshBuilder.CreateBox(name, {size:1000.0}, scene);
-  const material = new StandardMaterial(name, scene);
+/** Creates a grassy mesh on `scene` */
+function createGround(scene: Scene, name: string = 'grass.jpg'): Mesh {
+  // const material = new GridMaterial("grid", scene)
+  const props = 
+    { width: GROUND_SIZE
+    , height: GROUND_SIZE
+    , subdivisions: GROUND_DEPTH }
 
-  material.backFaceCulling = false;
-  material.reflectionTexture = new TEXTURES.CubeTexture(`textures/${name}`, scene);
-  material.reflectionTexture.coordinatesMode = TEXTURES.Texture.SKYBOX_MODE;
+  // const ground = MeshBuilder.CreateGround("ground1", props, scene)
+  const ground = MeshBuilder.CreateGround(name, props, scene)
+  const material = new StandardMaterial(name, scene)
+  material.alpha = 0.5;
+  const grass = new TEXTURES.Texture(`textures/${name}`, scene)
+  material.diffuseTexture = grass
+  material.reflectionTexture = grass
 
-  material.diffuseColor = new Color3(0, 0, 0);
-  material.specularColor = new Color3(0, 0, 0);
-  skybox.material = material;
-  return skybox;
+
+  // material.diffuseColor = new Color3(1, 0, 1)
+  // material.specularColor = new Color3(0.5, 0.6, 0.87)
+  // material.emissiveColor = new Color3(1, 1, 1)
+  // material.ambientColor = new Color3(0.23, 0.98, 0.53)
+  ground.material = material
+  return ground
 }
 
 
-function render( scene: Scene, sphere: Mesh ) {
+function createSkybox(scene, name = 'skybox'): Mesh {
+  const skybox = MeshBuilder.CreateBox(name, {size:1000.0}, scene)
+  const material = new StandardMaterial(name, scene)
+
+  material.backFaceCulling = false
+  material.reflectionTexture = new TEXTURES.CubeTexture(`textures/${name}`, scene)
+  material.reflectionTexture.coordinatesMode = TEXTURES.Texture.SKYBOX_MODE
+
+  material.diffuseColor = new Color3(0, 0, 0)
+  material.specularColor = new Color3(0, 0, 0)
+  skybox.material = material
+  return skybox
+}
+
+
+function render(scene: Scene, sphere: Mesh) {
     const listen = e => {
-      window.removeEventListener( 'keydown', listen );
-      handleKeydown( e.code, sphere );
+      // window.removeEventListener( 'keydown', listen )
+      handleKeydown( e.code, sphere )
     }
 
-    // window.removeEventListener( 'keydown', listen );
-    window.addEventListener( 'keydown', listen );
-    scene.render();
+    window.removeEventListener( 'keydown', listen )
+    window.addEventListener( 'keydown', listen )
+    scene.render()
 }
 
 
-function handleKeydown( code: string, sphere: Mesh ) {
+function handleKeydown(code: string, sphere: Mesh) {
   switch( code ) {
     case 'KeyD' :
-      sphere.position.x = sphere.position.x + 0.1;
-      break;
+      sphere.position.x = sphere.position.x + 0.1
+      break
 
     case 'KeyA' :
-      sphere.position.x = sphere.position.x - 0.1;
-      break;
+      sphere.position.x = sphere.position.x - 0.1
+      break
 
     case 'KeyW' :
-      sphere.position.y = sphere.position.y + 0.1;
-      break;
+      sphere.position.y = sphere.position.y + 0.1
+      break
 
     case 'KeyS' :
-      sphere.position.y = sphere.position.y - 0.1;
-      break;
+      sphere.position.y = sphere.position.y - 0.1
+      break
 
     case 'KeyQ' :
-      sphere.position.z = sphere.position.z + 0.1;
-      break;
+      sphere.position.z = sphere.position.z + 0.1
+      break
 
     case 'KeyE' :
-      sphere.position.z = sphere.position.z - 0.1;
-      break;
+      sphere.position.z = sphere.position.z - 0.1
+      break
     } 
 }
